8832c8409a87e55be3355e87f5d4be96997e2986
sobek1985 <sobek1985@gmail.com>
2016-03-07 13:09:39 +0000

    Add Universal App kiosk code

--- src/SitecoreKiosk/MainPage.xaml.cs
+++ src/SitecoreKiosk/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,8 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.Web.Http;
+using UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding;
 
 namespace SitecoreKiosk
 {
@@ -25,6 +26,43 @@ namespace SitecoreKiosk
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            PostFormToSitecore();
+        }
+
+        private void PostFormToSitecore()
+        {
+            var content = BuildJsonRequestContent();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var result = client.PostAsync(
+                        new Uri("http://192.168.1.112/sitecore/api/ssc/PersonSSC-Controllers/person"), content);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Handle error
+                throw;
+            }
+        }
+
+        private IHttpContent BuildJsonRequestContent()
+        {
+            IHttpContent content = new HttpStringContent(
+                    "{ \"firstname\": \"" + tbFirstName.Text +
+                    "\" , \"surname\": \" " + tbLastName.Text +
+                    " \", \"id\": \" " + tbEmail.Text +
+                    " \",\"email\":   \"" + tbEmail.Text + "\"}",
+                    UnicodeEncoding.Utf8,
+                    "application/json");
+
+            return content;
         }
     }
 }
